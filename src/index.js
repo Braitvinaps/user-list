aa07377b68e29049bb8680d1d629788b26ccac5b
Alexy <britvinaps@gmail.com>
2022-02-11 13:08:46 +0300

    polyfill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+import './index.css';
 
 
 ReactDOM.render(
